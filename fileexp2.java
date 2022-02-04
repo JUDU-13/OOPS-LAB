855883ef326260ef23b888cf4216362a4fe2156a
JAIDON GILL SHAJAN <84973988+JUDU-13@users.noreply.github.com>
2022-02-04 10:53:58 +0530

    Update fileexp2.java

--- fileexp2.java
+++ fileexp2.java
@@ -1,41 +1,51 @@
 package javacode;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class fileexp2 
  {
-  public static void main(String[] args) throws FileNotFoundException,IOException 
+  public static void main(String[] args) throws IOException 
    {
-    Scanner sc=new Scanner(System.in);
-	int i=0;
-	String word;
-	File fo = new File("text.txt");
+	File fo = new File("fileexp2.txt");
 		
-	  if(fo.exists())
-			System.out.println("File Exists");
-	  else
-			System.out.println("File Created ");
-	  
-	FileOutputStream fi=new FileOutputStream("text.txt");
-	System.out.print("\nEnter the sentence: ");
-	word=sc.nextLine();
-	byte[] Word=word.getBytes();
-	fi.write(Word);
-	fi.flush();
-	fi.close();
-	FileInputStream file=new FileInputStream("text.txt");
-	System.out.print("The entered sentence is: ");
-	
-	  while((i=file.read())!=-1)
-	   {
-		System.out.print((char) i);
+	 if(fo.exists())
+		System.out.println("File exists");
+	 else
+	  {
+		fo.createNewFile();
+		System.out.println("File created");
+	  }
+		
+	 try
+	  {
+	    FileOutputStream fi = new FileOutputStream("fileexp2.txt",true);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    System.out.println("Enter the data: ");
+		String s = br.readLine();
+		byte[] a = s.getBytes();
+	    fi.write(a);
+	    System.out.println("Data added");
 	   }
-	
-	 file.close();
-	 sc.close();
-	}
- }
+	 
+	 catch(Exception e)
+	   {
+		System.out.println("File not found");
+	   }
+		
+	int i=0;
+	FileInputStream fi = new FileInputStream("fileexp2.txt");
+	System.out.println(fi.available());
+		
+	 while((i=fi.read())!=-1)
+	   {
+		char x=(char)i;
+		System.out.print(x);
+	   }
+   }
+  }
